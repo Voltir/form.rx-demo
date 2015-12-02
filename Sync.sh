2cf492c7ba7b44e3d79d59ce8ccdced5616388d8
Nick <nick@stabtechs.com>
2015-12-01 16:48:05 -0800

    wip - Sync

--- Sync.sh
+++ Sync.sh
@@ -4,9 +4,13 @@ echo "Starting..."
 mkdir /tmp/formidable_demo/
 cp src/main/resources/* /tmp/formidable_demo/
 git checkout gh-pages
-mv /tmp/formidable_demo/* .
-echo "Syncing gh-pages"
-git commit -am "Sync to gh-pages"
+if [ $? -eq 0 ]; then
+  mv /tmp/formidable_demo/* .
+  echo "Syncing gh-pages"
+  git commit -am "Sync to gh-pages"
+else 
+  echo "FAILED TO CHECKOUT GH-PAGES!"
+fi
 #git push origin gh-pages
 #git checkout master
 #echo "Done"
